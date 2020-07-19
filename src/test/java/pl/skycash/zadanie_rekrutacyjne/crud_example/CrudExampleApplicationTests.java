6bcd49d3e0d30fc4bdbfc24f0aec08cd7b8c30a9
myusername <mrarczimedes@gmail.com>
2020-07-19 20:05:44 +0200

    first commit

---
+++ src/test/java/pl/skycash/zadanie_rekrutacyjne/crud_example/CrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skycash.zadanie_rekrutacyjne.crud_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
